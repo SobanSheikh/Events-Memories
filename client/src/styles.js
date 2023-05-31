3e51d6cd48f392aa64827cbd0061fe7640d5651d
Pro_Soban <92185551+SobanSheikh@users.noreply.github.com>
2023-05-31 17:19:03 +0500

    commit

---
+++ client/src/styles.js
@@ -0,0 +1,19 @@
+import { purple } from '@material-ui/core/colors';
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgb(12, 51, 130)',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+}));
